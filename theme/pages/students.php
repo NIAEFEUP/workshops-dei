e097bb1267b61867b3c221d2b39422117684aaf8
Sofia Silva <up201505628@fe.up.pt>
2018-02-06 18:04:39 +0000

    fixed box height

--- theme/pages/students.php
+++ theme/pages/students.php
@@ -1,183 +1,95 @@
 <?php include_once('common/header.php'); ?>
-<script src="../javascript/eb-extractor.js" defer></script>
+<!-- <script src="../javascript/eb-extractor.js" defer></script> -->
 
 <main role="main" class="container">
-    <div class="container">
-        <div class="row">
-            <!-- CAIXAS AQUI-->
+    <div class="row">
+        <!-- CAIXAS AQUI-->
 
-            <div class="col-xs-12 col-sm-6 col-lg-4">
-                <article>
-                    <div class="info">
-                        <h3>uc</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                            Nullam hendrerit, quam at porttitor vulputate.</p>
-                        <div class="container">
-                            <div class="row">
-                                <span class="col">
-                                    <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                <span class="col">
-                                    <i class="far fa-clock"></i>00h00</span>
-                                <div class="w-100"></div>
-                                <span class="col">
-                                    <i class="fas fa-user"></i>John Doe</span>
-                            </div>
+        <div class="top-buffer col-xs-12 col-sm-6 col-lg-4">
+            <article>
+                <div class="info">
+                    <h3>uc</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus. Nullam
+                        hendrerit, quam at porttitor vulputate.</p>
+                    <div class="container">
+                        <div class="row">
+                            <span class="col">
+                                <i class="far fa-calendar-alt"></i>00/00/0000</span>
+                            <span class="col">
+                                <i class="far fa-clock"></i>00h00</span>
+                            <div class="w-100"></div>
+                            <span class="col">
+                                <i class="fas fa-user"></i>John Doe</span>
                         </div>
                     </div>
-                    <input type="button" value="Select">
-                </article>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-lg-4">
-                <article>
-                    <div class="info">
-                        <h3>uc</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                            Nullam hendrerit, quam at porttitor vulputate.</p>
-                        <div class="container">
-                            <div class="row">
-                                <span class="col">
-                                    <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                <span class="col">
-                                    <i class="far fa-clock"></i>00h00</span>
-                                <div class="w-100"></div>
-                                <span class="col">
-                                    <i class="fas fa-user"></i>John Doe</span>
-                            </div>
-                        </div>
-                    </div>
-                    <input type="button" value="Select">
-                </article>
-            </div>
-            <!--
-            <div class="col-xs-12 col-sm-6 col-lg-4">
-                <article>
-                    <div class="info">
-                        <h3>uc</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                            Nullam hendrerit, quam at porttitor vulputate.</p>
-                        <div class="container">
-                            <div class="row">
-                                <span class="col">
-                                    <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                <span class="col">
-                                    <i class="far fa-clock"></i>00h00</span>
-                                <div class="w-100"></div>
-                                <span class="col">
-                                    <i class="fas fa-user"></i>John Doe</span>
-                            </div>
-                        </div>
-                    </div>
-                    <input type="button" value="Select">
-                </article>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-lg-4">
-                <article>
-                    <div class="info">
-                        <h3>uc</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                            Nullam hendrerit, quam at porttitor vulputate.</p>
-                        <div class="container">
-                            <div class="row">
-                                <span class="col">
-                                    <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                <span class="col">
-                                    <i class="far fa-clock"></i>00h00</span>
-                                <div class="w-100"></div>
-                                <span class="col">
-                                    <i class="fas fa-user"></i>John Doe</span>
-                            </div>
-                        </div>
-                    </div>
-                    <input type="button" value="Select">
-                </article>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-lg-4">
-                    <article>
-                        <div class="info">
-                            <h3>uc</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                                Nullam hendrerit, quam at porttitor vulputate.</p>
-                            <div class="container">
-                                <div class="row">
-                                    <span class="col">
-                                        <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                    <span class="col">
-                                        <i class="far fa-clock"></i>00h00</span>
-                                    <div class="w-100"></div>
-                                    <span class="col">
-                                        <i class="fas fa-user"></i>John Doe</span>
-                                </div>
-                            </div>
-                        </div>
-                        <input type="button" value="Select">
-                    </article>
                 </div>
-                <div class="col-xs-12 col-sm-6 col-lg-4">
-                    <article>
-                        <div class="info">
-                            <h3>uc</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                                Nullam hendrerit, quam at porttitor vulputate.</p>
-                            <div class="container">
-                                <div class="row">
-                                    <span class="col">
-                                        <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                    <span class="col">
-                                        <i class="far fa-clock"></i>00h00</span>
-                                    <div class="w-100"></div>
-                                    <span class="col">
-                                        <i class="fas fa-user"></i>John Doe</span>
-                                </div>
-                            </div>
+                <input type="button" value="Select">
+            </article>
+        </div>
+        <div class="top-buffer col-xs-12 col-sm-6 col-lg-4">
+            <article>
+                <div class="info">
+                    <h3>uc</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus. Nullam
+                        hendrerit, quam at porttitor vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Donec gravida enim ut porttitor maximus. Nullam hendrerit, quam at porttitor vulputate.</p>
+                    <div class="container">
+                        <div class="row">
+                            <span class="col">
+                                <i class="far fa-calendar-alt"></i>00/00/0000</span>
+                            <span class="col">
+                                <i class="far fa-clock"></i>00h00</span>
+                            <div class="w-100"></div>
+                            <span class="col">
+                                <i class="fas fa-user"></i>John Doe</span>
                         </div>
-                        <input type="button" value="Select">
-                    </article>
+                    </div>
                 </div>
-
-                <div class="col-xs-12 col-sm-6 col-lg-4">
-                        <article>
-                            <div class="info">
-                                <h3>uc</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                                    Nullam hendrerit, quam at porttitor vulputate.</p>
-                                <div class="container">
-                                    <div class="row">
-                                        <span class="col">
-                                            <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                        <span class="col">
-                                            <i class="far fa-clock"></i>00h00</span>
-                                        <div class="w-100"></div>
-                                        <span class="col">
-                                            <i class="fas fa-user"></i>John Doe</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <input type="button" value="Select">
-                        </article>
+                <input type="button" value="Select">
+            </article>
+        </div>
+        <div class="top-buffer col-xs-12 col-sm-6 col-lg-4">
+            <article>
+                <div class="info">
+                    <h3>uc</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus. Nullam
+                        hendrerit, quam at porttitor vulputate.</p>
+                    <div class="container">
+                        <div class="row">
+                            <span class="col">
+                                <i class="far fa-calendar-alt"></i>00/00/0000</span>
+                            <span class="col">
+                                <i class="far fa-clock"></i>00h00</span>
+                            <div class="w-100"></div>
+                            <span class="col">
+                                <i class="fas fa-user"></i>John Doe</span>
+                        </div>
                     </div>
-                    <div class="col-xs-12 col-sm-6 col-lg-4">
-                        <article>
-                            <div class="info">
-                                <h3>uc</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus.
-                                    Nullam hendrerit, quam at porttitor vulputate.</p>
-                                <div class="container">
-                                    <div class="row">
-                                        <span class="col">
-                                            <i class="far fa-calendar-alt"></i>00/00/0000</span>
-                                        <span class="col">
-                                            <i class="far fa-clock"></i>00h00</span>
-                                        <div class="w-100"></div>
-                                        <span class="col">
-                                            <i class="fas fa-user"></i>John Doe</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <input type="button" value="Select">
-                        </article>
-                    </div>
--->
+                </div>
+                <input type="button" value="Select">
+            </article>
+        </div>
 
+        <div class="top-buffer col-xs-12 col-sm-6 col-lg-4">
+            <article>
+                <div class="info">
+                    <h3>uc</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus. Nullam
+                        hendrerit, quam at porttitor vulputate.</p>
+                    <div class="container">
+                        <div class="row">
+                            <span class="col">
+                                <i class="far fa-calendar-alt"></i>00/00/0000</span>
+                            <span class="col">
+                                <i class="far fa-clock"></i>00h00</span>
+                            <div class="w-100"></div>
+                            <span class="col">
+                                <i class="fas fa-user"></i>John Doe</span>
+                        </div>
+                    </div>
+                </div>
+                <input type="button" value="Select">
+            </article>
         </div>
     </div>
 </main>

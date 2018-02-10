6e7757d743d4ff98be7600b4ae47c564caca815f
Edgar de Lemos Passos <edgarlpassos@gmail.com>
2018-02-10 15:27:14 +0000

    Frontend on speakers page

--- theme/pages/speakers.php
+++ theme/pages/speakers.php
@@ -1,14 +1,55 @@
 <?php include_once('common/header.php'); ?>
 
-<main>
-<div>
-<h1>Palestras</h1>
-<article>
-	<h2>Unidade Curricular</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida enim ut porttitor maximus. Nullam hendrerit, quam at porttitor vulputate.</p>
-	<button id="apply">Apply</button>
-</article>
-</div>
+<main role="main" class="container">
+	<section>
+		<div>
+			<h1>Palestras</h1>
+			<article class="speaker-call">
+				<h5>Worskhop de Docker</h5>
+				<div>
+					<span class="ws-field-title">Unidade Curricular:</span>
+					<span>ASSO</span>
+				</div>
+				<div>
+					<span class="ws-field-title">Descrição:</span>
+					<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+						occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
+				</div>
+			</article>
+			<article class="speaker-call">
+				<h5>Worskhop de Javascript</h5>
+				<div>
+					<span class="ws-field-title">Unidade Curricular:</span>
+					<span>CGRA</span>
+				</div>
+				<div>
+					<span class="ws-field-title">Descrição:</span>
+					<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+						occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
+				</div>
+			</article>
+			<article class="speaker-call">
+				<a href="#">
+					<h5>PROLOG avançado</h5>
+					<div>
+						<span class="ws-field-title">Unidade Curricular:</span>
+						<span>PLOG</span>
+					</div>
+					<div>
+						<span class="ws-field-title">Descrição:</span>
+						<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+							occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
+					</div>
+				</a>
+			</article>
+		</div>
+	</section>
 </main>
 
 <?php include_once('common/footer.php'); ?>

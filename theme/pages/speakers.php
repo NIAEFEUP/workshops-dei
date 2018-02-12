7e2441efce4ab9ebcea5b090d373db8ef37592d5
Sofia Silva <up201505628@fe.up.pt>
2018-02-12 16:20:14 +0000

    speakers frontend :white_check_mark:

--- theme/pages/speakers.php
+++ theme/pages/speakers.php
@@ -5,50 +5,60 @@
 		<div>
 			<h1>Palestras</h1>
 			<article class="speaker-call">
-				<h5>Worskhop de Docker</h5>
-				<div>
-					<span class="ws-field-title">Unidade Curricular:</span>
-					<span>ASSO</span>
-				</div>
-				<div>
-					<span class="ws-field-title">Descrição:</span>
-					<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-						occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
-				</div>
-			</article>
-			<article class="speaker-call">
-				<h5>Worskhop de Javascript</h5>
-				<div>
-					<span class="ws-field-title">Unidade Curricular:</span>
-					<span>CGRA</span>
-				</div>
-				<div>
-					<span class="ws-field-title">Descrição:</span>
-					<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-						occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
-				</div>
-			</article>
-			<article class="speaker-call">
-				<a href="#">
-					<h5>PROLOG avançado</h5>
-					<div>
-						<span class="ws-field-title">Unidade Curricular:</span>
-						<span>PLOG</span>
+				<div class="row">
+					<div class="col-xs-12 col-sm-8 col-lg-10">
+						<h5>Worskhop de Docker</h5>
+						<div>
+							<span class="ws-field-title">Unidade Curricular:</span>
+							<span>ASSO</span>
+						</div>
+						<span class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+							occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
 					</div>
-					<div>
-						<span class="ws-field-title">Descrição:</span>
-						<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+					<div class="col-xs-12 col-sm-4 col-lg-2 v-center">
+						<input type="button" value="Apply">
+					</div>
+				</div>
+			</article>
+			<hr>
+			<article class="speaker-call">
+				<div class="row">
+					<div class="col-xs-12 col-sm-8 col-lg-10">
+						<h5>Worskhop de Javascript</h5>
+						<div>
+							<span class="ws-field-title">Unidade Curricular:</span>
+							<span>CGRA</span>
+						</div>
+						<span class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+							occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-lg-2 v-center">
+						<input type="button" value="Apply">
+					</div>
+			</article>
+			<hr>
+			<article class="speaker-call">
+				<div class="row">
+					<div class="col-xs-12 col-sm-8 col-lg-10">
+						<h5>PROLOG avançado</h5>
+						<div>
+							<span class="ws-field-title">Unidade Curricular:</span>
+							<span>PLOG</span>
+						</div>
+						<span class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
 							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
 							occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
 					</div>
-				</a>
+					<div class="col-xs-12 col-sm-4 col-lg-2 v-center">
+						<input type="button" value="Apply">
+					</div>
 			</article>
-		</div>
+			</div>
 	</section>
 </main>
 
